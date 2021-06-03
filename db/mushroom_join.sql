8aaddfb794e582ac8d757a018375872777e6da5c
hayleadoty <hayleadoty@gmail.com>
2021-06-02 20:00:01 -0500

    Adding updated join file.

--- db/mushroom_join.sql
+++ db/mushroom_join.sql
@@ -1,7 +1,7 @@
 -- Importing CSV file into mushroom database
 COPY raw_mushroom FROM '/Library/PostgreSQL/11/bin/Database/mushrooms.csv' DELIMITER ',' CSV HEADER;
 
--- Clean mushroom table was exported to mushroom database via Pandas in our machine learning model
+-- Clean mushroom table will be exported from our machine learning model to SQL db via Pandas with .to_sql
 
 -- Joining the raw & clean mushroom tables together on the mushroom_id column
 SELECT * 
